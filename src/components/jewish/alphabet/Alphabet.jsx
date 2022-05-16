199add9b1545dceb1e64a6498db60079278a759e
alexseveneight <alexseveneight@gmail.com>
2022-05-16 21:27:39 +0300

    ..

--- src/components/jewish/alphabet/Alphabet.jsx
+++ src/components/jewish/alphabet/Alphabet.jsx
@@ -6,7 +6,7 @@ const Alphabet = () => {
     const [letters, setLetters] = useState([
         { aleph: 'ℵ', html: '&#8501;', letter: 'אָלֶף', translit: 'ʾ', prononc: 'алеф', number: 1 },
         { beth: 'ℶ', html: '&#8502;', letter: 'בֵּית', translit: ['b', 'ḇ'], number: 2 },
-        { beth: 'ℷ', html: '&#8503;', letter: 'גִימֶל', translit: ['g', 'ḡ'], number: 3 }
+        { gimel: 'ℷ', html: '&#8503;', letter: 'גִימֶל', translit: ['g', 'ḡ'], number: 3 }
     ]);
 
     return (
---
+++ src/components/jewish/alphabet/alphabetItem/AlphabetTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AlphabetTable.css';
+
+const AlphabetTable = () => {
+    return (
+        <div className="AlphabetTable">
+            
+        </div>
+    );
+};
+
+export default AlphabetTables;
